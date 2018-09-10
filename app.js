901f9e20a985584f8d1dba4ec70f3a2e6754a4ed
FrozenTear7 <RainbowPablo7@gmail.com>
2018-09-10 14:07:51 +0200

    Save quotes in the database and allow them to be randomly read

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const Discord = require('discord.js')
+const mongoose = require('mongoose')
 const config = require('./config.json')
+const Quote = require('./quoteSchema')
 
 const client = new Discord.Client()
+
+mongoose.connect('mongodb://@ds249992.mlab.com:49992/quotebot-db', config.dbOptions)
+
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('Connection open')
+})
 
 client.on('ready', () => {
   console.log('Ready!')
@@ -9,7 +19,28 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content.match(/".*" ~.*/)) {
-    message.channel.send('Quote: ' + message.content.match(/".*"/)[0] + ', author: ' + message.content.match(/~.*/)[0].substring(1))
+    const newQuote = new Quote({
+      quote: message.content.match(/".*"/)[0],
+      author: message.content.match(/~.*/)[0].substring(1),
+    })
+    newQuote.save((err) => {
+      if (err)
+        message.channel.send(err)
+      else
+        message.channel.send('SAVED -> Quote: ' + message.content.match(/".*"/)[0] + ', author: ' + message.content.match(/~.*/)[0].substring(1))
+    })
+  } else if (message.content.match(/!quote .*/)) {
+    Quote.find({author: message.content.match(/!quote .*/)[0].substring(7)})
+      .exec((err, quotes) => {
+          if (err)
+          message.channel.send(err)
+        else {
+          if (quotes.length === 0)
+            message.channel.send('This author has no quotes yet!')
+          else
+            message.channel.send(quotes[Math.floor(Math.random() * (quotes.length))].quote)
+        }
+      })
   }
 })
 
---
+++ quoteSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const quoteSchema = mongoose.Schema({
+  author: {type: String, required: true},
+  quote: {type: String, required: true}
+})
+
+const Quote = mongoose.model('Quote', quoteSchema)
+
+module.exports = Quote
