c36cb3bcbcae7fe06c29bf7ec21944ce28185884
FrozenTear7 <RainbowPablo7@gmail.com>
2018-09-10 16:42:51 +0200

    Heroku changes

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.listen(process.env.PORT || 8080)
 const client = new Discord.Client()
 
 mongoose.connect('mongodb://@ds249992.mlab.com:49992/quotebot-db', {
-  "user": process.env.DBUSER,
-  "pass": process.env.DBPASS,
-  "useNewUrlParser": true
+  'user': process.env.DBUSER,
+  'pass': process.env.DBPASS,
+  'useNewUrlParser': true,
 })
 
 let db = mongoose.connection
@@ -38,11 +38,11 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
-  console.log(message.channel.guild.name)
   if (message.content.match(/".*" ~.*/)) {
     const newQuote = new Quote({
       quote: message.content.match(/".*"/)[0],
       author: message.content.match(/~.*/)[0].substring(1),
+      server: message.channel.guild.name,
     })
     newQuote.save((err) => {
       if (err)
@@ -53,9 +53,9 @@ client.on('message', message => {
         })
     })
   } else if (message.content.match(/!quote .*/)) {
-    Quote.find({author: message.content.match(/!quote .*/)[0].substring(7)})
+    Quote.find({server: message.channel.guild.name, author: message.content.match(/!quote .*/)[0].substring(7)})
       .exec((err, quotes) => {
-          if (err)
+        if (err)
           message.channel.send(err)
         else {
           if (quotes.length === 0)
