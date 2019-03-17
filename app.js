7a103021de32bdabae19a6a456997be127fae387
FrozenTear7 <RainbowPablo7@gmail.com>
2019-03-17 22:06:21 +0100

    Errors still repeating

--- app.js
+++ app.js
@@ -43,31 +43,46 @@ client.on('ready', () => {
 client.on('error', console.log)
 
 client.on('message', message => {
-    if (message.channel.type === 'dm') {
-      return message.channel.send({
+  if (!message.author.bot && message.content.match(/^['"].+['"] *~ *.+/) && message.channel.type !== 'dm') {
+    if (message.content.length >= 256) {
+      message.channel.send({
         embed: {
-          color: 0xff0000,
-          description: 'Channel only!',
+          color: 3447003,
+          description: 'Message must be shorter than 256 characters!',
         },
       }).then(msg => {
         msg.delete(15000)
       })
     } else {
-      if (!message.author.bot && message.content.match(/^['"].+['"] *~ *.+/)) {
-        if (message.content.length >= 256) {
+      Author.findOne({
+        server: message.channel.guild.name,
+        names: {$in: message.content.match(/~ *.+/)[0].substring(message.content.match(/~ */)[0].length)},
+      }, (err, author) => {
+        if (err)
           message.channel.send({
             embed: {
               color: 3447003,
-              description: 'Message must be shorter than 256 characters!',
+              description: err.errmsg,
             },
           }).then(msg => {
             msg.delete(15000)
           })
-        } else {
-          Author.findOne({
-            server: message.channel.guild.name,
-            names: {$in: message.content.match(/~ *.+/)[0].substring(message.content.match(/~ */)[0].length)},
-          }, (err, author) => {
+        else if (!message.content.match(/^['"].+['"]/))
+          message.channel.send({
+            embed: {
+              color: 3447003,
+              description: 'Something went wrong',
+            },
+          }).then(msg => {
+            msg.delete(15000)
+          })
+        else if (author) {
+          const newQuote = new Quote({
+            quote: message.content.match(/^['"].+['"]/)[0],
+            author: author,
+          })
+
+          newQuote.save((err) => {
             if (err)
               message.channel.send({
                 embed: {
@@ -77,19 +92,45 @@ client.on('message', message => {
               }).then(msg => {
                 msg.delete(15000)
               })
-            else if (!message.content.match(/^['"].+['"]/))
+            else
               message.channel.send({
                 embed: {
                   color: 3447003,
-                  description: 'Something went wrong',
+                  title: 'Saved',
+                  fields: [
+                    {
+                      name: 'Quote: ' + newQuote.quote + ', author: ' + author.names[0],
+                      value: 'quoteId: ' + newQuote._id + ', authorId: ' + author._id,
+                    },
+                  ],
                 },
               }).then(msg => {
                 msg.delete(15000)
               })
-            else if (author) {
+          })
+        } else {
+          let authorNames = []
+          authorNames.push(message.content.match(/~.+/)[0].substring(1))
+
+          const newAuthor = new Author({
+            names: authorNames,
+            server: message.channel.guild.name,
+          })
+
+          newAuthor.save((err) => {
+            if (err)
+              message.channel.send({
+                embed: {
+                  color: 3447003,
+                  description: err.errmsg,
+                },
+              }).then(msg => {
+                msg.delete(15000)
+              })
+            else {
               const newQuote = new Quote({
-                quote: message.content.match(/^['"].+['"]/)[0],
-                author: author,
+                quote: message.content.match(/^'[^']+'/)[0],
+                author: newAuthor,
               })
 
               newQuote.save((err) => {
@@ -109,8 +150,8 @@ client.on('message', message => {
                       title: 'Saved',
                       fields: [
                         {
-                          name: 'Quote: ' + newQuote.quote + ', author: ' + author.names[0],
-                          value: 'quoteId: ' + newQuote._id + ', authorId: ' + author._id,
+                          name: 'Quote added!',
+                          value: 'quoteId: ' + newQuote._id + ', authorId: ' + newAuthor._id,
                         },
                       ],
                     },
@@ -118,66 +159,13 @@ client.on('message', message => {
                     msg.delete(15000)
                   })
               })
-            } else {
-              let authorNames = []
-              authorNames.push(message.content.match(/~.+/)[0].substring(1))
-
-              const newAuthor = new Author({
-                names: authorNames,
-                server: message.channel.guild.name,
-              })
-
-              newAuthor.save((err) => {
-                if (err)
-                  message.channel.send({
-                    embed: {
-                      color: 3447003,
-                      description: err.errmsg,
-                    },
-                  }).then(msg => {
-                    msg.delete(15000)
-                  })
-                else {
-                  const newQuote = new Quote({
-                    quote: message.content.match(/^'[^']+'/)[0],
-                    author: newAuthor,
-                  })
-
-                  newQuote.save((err) => {
-                    if (err)
-                      message.channel.send({
-                        embed: {
-                          color: 3447003,
-                          description: err.errmsg,
-                        },
-                      }).then(msg => {
-                        msg.delete(15000)
-                      })
-                    else
-                      message.channel.send({
-                        embed: {
-                          color: 3447003,
-                          title: 'Saved',
-                          fields: [
-                            {
-                              name: 'Quote added!',
-                              value: 'quoteId: ' + newQuote._id + ', authorId: ' + newAuthor._id,
-                            },
-                          ],
-                        },
-                      }).then(msg => {
-                        msg.delete(15000)
-                      })
-                  })
-                }
-              })
             }
           })
         }
-      }
+      })
     }
-  },
-)
+  }
+})
 
 client.registry
   .registerGroup('util', 'Util')
